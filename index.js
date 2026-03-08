c112bfa73beff51406039f2692801020069dc6ce
alexcactus0 <217364473+alexcactus0@users.noreply.github.com>
2026-03-08 16:05:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile("contact-me.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    fs.readFile("404.html", (err, data) => {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080");
+});
